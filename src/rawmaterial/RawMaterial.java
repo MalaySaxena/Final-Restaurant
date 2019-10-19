a1bdcd6aefa0d52437b63fcaeb86c95397a33f18
Ajinkya Taranekar <ajinkyataranekar@gmail.com>
2019-10-19 19:13:26 +0530

    added ingredients

--- src/rawmaterial/RawMaterial.java
+++ src/rawmaterial/RawMaterial.java
@@ -7,7 +7,52 @@ public class RawMaterial {
 	
 	RawMaterial()
 	{
-		ingredients.put(50, arg1)
+		//vegetables
+		ingredients.put(50, "Palak");
+		ingredients.put(50, "Cauliflower");
+		ingredients.put(50, "Cabbage");
+		ingredients.put(50, "Tomato");
+		ingredients.put(50, "Methi");
+		ingredients.put(50, "Dhaniya");
+		ingredients.put(50, "Carrot");
+		ingredients.put(50, "Potato");
+		ingredients.put(50, "Onion");
+		ingredients.put(50, "Green-Capsicum");
+		ingredients.put(50, "Red-Capsicum");
+		ingredients.put(50, "Yellow-Capsicum");
+		ingredients.put(50, "Mushroom");
+		
+		
+		// type of flour
+		ingredients.put(50, "Whole-wheat");
+		ingredients.put(50, "Normal atta");
+		ingredients.put(50, "Besan");
+		
+		//sauce
+		ingredients.put(50, "Ketchup");
+		ingredients.put(50, "Vinegar");
+		ingredients.put(50, "Soya Sauce");
+		ingredients.put(50, "Mayonnese");
+		ingredients.put(50, "Red Chilli Sauce");
+		
+		ingredients.put(50, "Sandwich Bread");
+		ingredients.put(50, "Cheese");
+		ingredients.put(50, "Pizza bread");
+		ingredients.put(50, "Salt");
+		ingredients.put(50, "Garam Masala");
+		ingredients.put(50, "Turmeric Powder");
+		ingredients.put(50, "Red Chilli Powder");
+		ingredients.put(50, "Vinegar");
+		ingredients.put(50, "Whole-wheat");
+		ingredients.put(50, "Whole-wheat");
+		ingredients.put(50, "Whole-wheat");
+		ingredients.put(50, "Whole-wheat");
+		ingredients.put(50, "Whole-wheat");
+		ingredients.put(50, "Whole-wheat");
+		ingredients.put(50, "Whole-wheat");
+		ingredients.put(50, "Whole-wheat");
+		ingredients.put(50, "Whole-wheat");
+		ingredients.put(50, "Whole-wheat");
 	}
 	
 }
