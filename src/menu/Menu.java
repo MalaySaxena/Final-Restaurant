8a807c86e847f329c36e302a7fc4d9798280c09a
Malay <44743128+MalaySaxena@users.noreply.github.com>
2019-10-19 19:11:58 +0530

    third

---
+++ src/menu/Menu.java
@@ -0,0 +1,60 @@
+package menu;
+
+import java.util.ArrayList;
+
+import rawmaterial.FoodItem;
+
+public class Menu {
+	ArrayList<FoodItem> menu = new ArrayList<FoodItem>(16);
+	
+	FoodItem food = null;
+	
+	//1
+	food = new FoodItem("Sandwich",30);
+	
+	//1
+    food = new FoodItem("Sandwich",30);
+		
+    //1
+	food = new FoodItem("Sandwich",30);	
+	
+	//3
+    food = new FoodItem("Sandwich",30);	
+	
+    //4
+    food = new FoodItem("Sandwich",30);	
+
+    //5
+    food = new FoodItem("Sandwich",30);	
+    
+    //6
+    food = new FoodItem("Sandwich",30);	
+
+    //7
+    food = new FoodItem("Sandwich",30);	
+
+    //8
+    food = new FoodItem("Sandwich",30);	
+
+    //9
+    food = new FoodItem("Sandwich",30);	
+
+    //10
+    food = new FoodItem("Sandwich",30);	
+
+    //11
+    food = new FoodItem("Sandwich",30);	
+
+    //12
+    food = new FoodItem("Sandwich",30);	
+
+    //13
+    food = new FoodItem("Sandwich",30);	
+
+    //14
+    food = new FoodItem("Sandwich",30);	
+
+    //15
+    food = new FoodItem("Sandwich",30);	
+
+}
--- src/rawmaterial/s.java
+++
@@ -1,5 +0,0 @@
-package rawmaterial;
-
-public class s {
-
-}
