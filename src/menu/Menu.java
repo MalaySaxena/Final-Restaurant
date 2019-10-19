c93f8e4ad52df2b9c2c63f648fb5ef25ef219028
Ajinkya Taranekar <ajinkyataranekar@gmail.com>
2019-10-19 20:25:43 +0530

    Completed Ingredients and Menu

--- src/menu/Menu.java
+++ src/menu/Menu.java
@@ -5,66 +5,213 @@ import java.util.ArrayList;
 import rawmaterial.FoodItem;
 
 public class Menu {
-	ArrayList<FoodItem> menu = new ArrayList<FoodItem>(17);
-	
+	ArrayList<FoodItem> menu = new ArrayList<FoodItem>(21);
+
 	{
 		FoodItem food = null;
-		
-		
-		//1
-		food = new FoodItem("Sandwich",30);
-		food.addRawMaterial("Onion",4);
-		food.addRawMaterial("Sandwhich Bread",2);
+
+		// 1 Sandwich
+		// i)
+		food = new FoodItem("Cheese Sandwich", 30);
+		food.addRawMaterial("Cheese", 2);
+		food.addRawMaterial("Black pepper", 1);
+		food.addRawMaterial("Sandwhich Bread", 2);
+		menu.add(food);
+		// ii)
+		food = new FoodItem("Veg Sandwich", 30);
+		food.addRawMaterial("Tomato", 2);
+		food.addRawMaterial("Onion", 2);
+		food.addRawMaterial("Cabbage", 1);
+		food.addRawMaterial("Cucumber", 1);
+		food.addRawMaterial("Black pepper", 1);
+		food.addRawMaterial("Sandwhich Bread", 2);
+		menu.add(food);
+
+		// 2 Pizza
+		// i)
+		food = new FoodItem("Onion Pizza", 30);
+		food.addRawMaterial("Cheese", 2);
+		food.addRawMaterial("Black pepper", 1);
+		food.addRawMaterial("Onion", 1);
+		food.addRawMaterial("Pizza Sauce", 1);
+		food.addRawMaterial("Pizza Bread", 1);
+		menu.add(food);
+
+		// ii)
+		food = new FoodItem("Margheritta", 30);
+		food.addRawMaterial("Cheese", 2);
+		food.addRawMaterial("Pizza Sauce", 1);
+		food.addRawMaterial("Black pepper", 1);
+		food.addRawMaterial("Pizza Bread", 1);
+		menu.add(food);
+
+		// iii)
+		food = new FoodItem("Farm Fresh Pizza", 30);
+		food.addRawMaterial("Cheese", 2);
+		food.addRawMaterial("Pizza Sauce", 1);
+		food.addRawMaterial("Black pepper", 1);
+		food.addRawMaterial("Onion", 1);
+		food.addRawMaterial("Green-Capsicum", 1);
+		food.addRawMaterial("Yellow-Capsicum", 1);
+		food.addRawMaterial("Red-Capsicum", 1);
+		food.addRawMaterial("Pizza Bread", 1);
+		menu.add(food);
+
+		// 3 Chinese
+		// i)
+		food = new FoodItem("Noodles", 30);
+		food.addRawMaterial("Dry Noodles", 2);
+		food.addRawMaterial("Green Capsicum", 1);
+		food.addRawMaterial("Onion", 1);
+		food.addRawMaterial("Vinegar", 1);
+		food.addRawMaterial("Red Chilli Sauce", 1);
+		food.addRawMaterial("Oil", 1);
+		menu.add(food);
+
+		// ii)
+		food = new FoodItem("Manchurian", 30);
+		food.addRawMaterial("Manchrian balls", 2);
+		food.addRawMaterial("Red Chilli Sauce", 1);
+		food.addRawMaterial("Green Capsicum", 1);
+		food.addRawMaterial("Oil", 1);
+		food.addRawMaterial("Onion", 1);
+		food.addRawMaterial("Vinegar", 1);
+		menu.add(food);
+
+		// iii)
+		food = new FoodItem("Fried Rice", 30);
+		food.addRawMaterial("Boiled Rice", 2);
+		food.addRawMaterial("Red Chilli Sauce", 1);
+		food.addRawMaterial("Green Capsicum", 1);
+		food.addRawMaterial("Oil", 1);
+		food.addRawMaterial("Onion", 1);
+		food.addRawMaterial("Vinegar", 1);
+		menu.add(food);
+		// 3 Chinese
+		// i)
+		food = new FoodItem("Noodles", 30);
+		food.addRawMaterial("Dry Noodles", 2);
+		food.addRawMaterial("Oil", 1);
+		food.addRawMaterial("Green Capsicum", 1);
+		food.addRawMaterial("Onion", 1);
+		food.addRawMaterial("Vinegar", 1);
+		food.addRawMaterial("Red Chilli Sauce", 1);
+		menu.add(food);
+
+		// ii)
+		food = new FoodItem("Manchurian", 30);
+		food.addRawMaterial("Manchrian balls", 2);
+		food.addRawMaterial("Red Chilli Sauce", 1);
+		food.addRawMaterial("Oil", 1);
+		food.addRawMaterial("Green Capsicum", 1);
+		food.addRawMaterial("Onion", 1);
+		food.addRawMaterial("Vinegar", 1);
+		menu.add(food);
+
+		// iii)
+		food = new FoodItem("Fried Rice", 30);
+		food.addRawMaterial("Boiled Rice", 2);
+		food.addRawMaterial("Red Chilli Sauce", 1);
+		food.addRawMaterial("Oil", 1);
+		food.addRawMaterial("Green Capsicum", 1);
+		food.addRawMaterial("Onion", 1);
+		food.addRawMaterial("Vinegar", 1);
 		menu.add(food);
 		
+		// 4 Main Course 
+		// i)
+		food = new FoodItem("Palak Paneer", 30);
+		food.addRawMaterial("Paneer", 2);
+		food.addRawMaterial("Oil", 1);
+		food.addRawMaterial("Palak", 1);
+		food.addRawMaterial("Onion", 1);
+		food.addRawMaterial("Salt", 1);
+		food.addRawMaterial("Red Chilli Powder", 1);
+		food.addRawMaterial("Turmeric Powder", 1);
+		food.addRawMaterial("Garam Masala Powder", 1);
+		menu.add(food);
+
+		// ii)
+		food = new FoodItem("Paneer Butter Masala", 30);
+		food.addRawMaterial("Paneer", 2);
+		food.addRawMaterial("Oil", 1);
+		food.addRawMaterial("Tomato", 2);
+		food.addRawMaterial("Butter", 2);
+		food.addRawMaterial("Onion", 2);
+		food.addRawMaterial("Salt", 1);
+		food.addRawMaterial("Red Chilli Powder", 1);
+		food.addRawMaterial("Turmeric Powder", 1);
+		food.addRawMaterial("Garam Masala Powder", 1);
+		menu.add(food);
+
+		// iii)
+		food = new FoodItem("Cauliflower Masala", 30);
+		food.addRawMaterial("Cauliflower", 2);
+		food.addRawMaterial("Tomato", 2);
+		food.addRawMaterial("Oil", 1);
+		food.addRawMaterial("Butter", 2);
+		food.addRawMaterial("Onion", 2);
+		food.addRawMaterial("Salt", 1);
+		food.addRawMaterial("Red Chilli Powder", 1);
+		food.addRawMaterial("Turmeric Powder", 1);
+		food.addRawMaterial("Garam Masala Powder", 1);
+		menu.add(food);
 		
-		//1
-	    food = new FoodItem("Pizza",30);
-			
-	    //1
-		food = new FoodItem("Sandwich",30);	
+		// iv)
+		food = new FoodItem("Aloo Masala", 30);
+		food.addRawMaterial("Potato", 2);
+		food.addRawMaterial("Tomato", 2);
+		food.addRawMaterial("Butter", 2);
+		food.addRawMaterial("Oil", 1);
+		food.addRawMaterial("Onion", 2);
+		food.addRawMaterial("Salt", 1);
+		food.addRawMaterial("Red Chilli Powder", 1);
+		food.addRawMaterial("Turmeric Powder", 1);
+		food.addRawMaterial("Garam Masala Powder", 1);
+		menu.add(food);
 		
-		//3
-	    food = new FoodItem("Sandwich",30);	
+		//Roti
+		// i)
+		food = new FoodItem("Plain Roti", 5);
+		food.addRawMaterial("Normal Atta", 2);
+		food.addRawMaterial("Salt", 1);
+		menu.add(food);
+
+		// ii)
+		food = new FoodItem("Butter Roti", 7);
+		food.addRawMaterial("Butter", 2);
+		food.addRawMaterial("Normal Atta", 2);
+		food.addRawMaterial("Salt", 1);
+		menu.add(food);
+
+		// iii)
+		food = new FoodItem("Butter Naan", 15);
+		food.addRawMaterial("Butter", 2);
+		food.addRawMaterial("Whole Atta", 2);
+		food.addRawMaterial("Salt", 1);
+		menu.add(food);
 		
-	    //4
-	    food = new FoodItem("Sandwich",30);	
-	
-	    //5
-	    food = new FoodItem("Sandwich",30);	
-	    
-	    //6
-	    food = new FoodItem("Sandwich",30);	
-	
-	    //7
-	    food = new FoodItem("Sandwich",30);	
-	
-	    //8
-	    food = new FoodItem("Sandwich",30);	
-	
-	    //9
-	    food = new FoodItem("Sandwich",30);	
-	
-	    //10
-	    food = new FoodItem("Sandwich",30);	
-	
-	    //11
-	    food = new FoodItem("Sandwich",30);	
-	
-	    //12
-	    food = new FoodItem("Sandwich",30);	
-	
-	    //13
-	    food = new FoodItem("Sandwich",30);	
-	
-	    //14
-	    food = new FoodItem("Sandwich",30);	
-	
-	    //15
-	    food = new FoodItem("Sandwich",30);	
-	    
-	    //16
-	    food = new FoodItem("Sandwich",30);	
+		// iv)
+		food = new FoodItem("Tandoori Roti", 10);
+		food.addRawMaterial("Butter", 2);
+		food.addRawMaterial("Whole Atta", 2);
+		food.addRawMaterial("Salt", 1);
+		menu.add(food);			
+		
+		//Rice
+		// i)
+		food = new FoodItem("Plain Rice", 30);
+		food.addRawMaterial("Boiled Rice", 2);
+		food.addRawMaterial("Salt", 1);
+		menu.add(food);
+
+		// ii)
+		food = new FoodItem("Jeera Rice", 50);
+		food.addRawMaterial("Boiled Rice", 2);
+		food.addRawMaterial("Oil", 1);
+		food.addRawMaterial("Salt", 1);
+		food.addRawMaterial("Jeera", 1);
+		menu.add(food);
+		
 	}
 }
-	
--- src/rawmaterial/RawMaterial.java
+++ src/rawmaterial/RawMaterial.java
@@ -3,50 +3,61 @@ package rawmaterial;
 import java.util.Hashtable;
 
 public class RawMaterial {
-	static Hashtable<String,Integer> ingredients = new Hashtable<String,Integer>();
+	static Hashtable<String, Integer> ingredients = new Hashtable<String,Integer>(36);
 	
 	RawMaterial()
 	{
 		//vegetables
-		ingredients.put( "Palak",50);
+		ingredients.put("Palak",50);
 		ingredients.put("Cauliflower",50);
 		ingredients.put("Cabbage",50);
 		ingredients.put("Tomato",50);
-		ingredients.put(50, "Methi");
-		ingredients.put(50, "Dhaniya");
-		ingredients.put(50, "Carrot");
-		ingredients.put(50, "Potato");
-		ingredients.put(50, "Onion");
-		ingredients.put(50, "Green-Capsicum");
-		ingredients.put(50, "Red-Capsicum");
-		ingredients.put(50, "Yellow-Capsicum");
-		ingredients.put(50, "Mushroom");
+		ingredients.put("Methi",50);
+		ingredients.put("Dhaniya",50);
+		ingredients.put("Cucumber",50);
+		ingredients.put("Carrot",50);
+		ingredients.put("Potato",50);
+		ingredients.put("Onion",50);
+		ingredients.put("Green-Capsicum",50);
+		ingredients.put("Red-Capsicum",50);
+		ingredients.put("Yellow-Capsicum",50);
+		ingredients.put("Mushroom",50);
 		
 		
 		// type of flour
-		ingredients.put(50, "Whole-wheat");
-		ingredients.put(50, "Normal atta");
-		ingredients.put(50, "Besan");
+		ingredients.put("Whole-wheat",50);
+		ingredients.put("Normal atta",50);
+		ingredients.put("Besan",50);
 		
 		//sauce
-		ingredients.put(50, "Ketchup");
-		ingredients.put(50, "Vinegar");
-		ingredients.put(50, "Soya Sauce");
-		ingredients.put(50, "Mayonnese");
-		ingredients.put(50, "Red Chilli Sauce");
+		ingredients.put("Ketchup",50);
+		ingredients.put("Vinegar",50);
+		ingredients.put("Soya Sauce",50);
+		ingredients.put("Mayonnese",50);
+		ingredients.put("Pizza Sauce",50);
+		ingredients.put("Red Chilli Sauce",50);
 		
 		//spices
-		ingredients.put(50, "Salt");
-		ingredients.put(50, "Garam Masala");
-		ingredients.put(50, "Turmeric Powder");
-		ingredients.put(50, "Red Chilli Powder");
+		ingredients.put("Salt",50);
+		ingredients.put("Garam Masala",50);
+		ingredients.put("Turmeric Powder",50);
+		ingredients.put("Red Chilli Powder",50);
+		ingredients.put("Jeera",50);
 		
 		//bakery
-		ingredients.put(50, "Sandwich Bread");
-		ingredients.put(50, "Cheese");
-		ingredients.put(50, "Pizza bread");
+		ingredients.put("Sandwich Bread",50);
+		ingredients.put("Cheese",50);
+		ingredients.put("Pizza bread",50);
+		ingredients.put("Butter",50);
+			
 		
 		// further ingredients
+		ingredients.put("Dry Noodles",50);
+		ingredients.put("Machurian balls",50);
+		ingredients.put("Boiled Rice",50);
+		ingredients.put("Oil",50);
+		
+		
 		
 	}
 	
