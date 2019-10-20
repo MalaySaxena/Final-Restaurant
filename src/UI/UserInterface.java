0f075ad8440dd4b55f3bf8426e254a46b6fe3621
Ajinkya Taranekar <ajinkyataranekar@gmail.com>
2019-10-20 09:35:10 +0530

    Started admin side user interface

--- src/UI/UserInterface.java
+++ src/UI/UserInterface.java
@@ -4,7 +4,6 @@ import java.util.Random;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
-
 import menu.Menu;
 import order.Order;
 import order.Customer;
@@ -49,31 +48,29 @@ public class UserInterface {
 						System.out.println("Veronica=> \t May I have your name please.");
 						System.out.print("Customer=> \t ");
 						customer.setCustomerName(keyboard.next());
-						
+
 						System.out.println("\nVeronica=> \t Also, May I have your mobile number please.");
-						System.out.print(customer.getCustomerName()+" ji=> \t ");
+						System.out.print(customer.getCustomerName() + " ji=> \t ");
 						customer.setCustomerMobileNumber(keyboard.next());
-						
+
 						System.out.println("\nVeronica=> \t Enter Order please.");
 						Random r = new Random();
 						int tableNumber = r.nextInt(10) + 1;
 						System.out.println("Veronica=> \t Your Table number is " + tableNumber);
-						
+
 						System.out.println("Veronica=> \t From Main menu, what you would like to have?");
-						while(true) {
-							System.out.print(customer.getCustomerName()+" ji=> \t ");
+						while (true) {
+							System.out.print(customer.getCustomerName() + " ji=> \t ");
 							int foodItemNumber = keyboard.nextInt();
 							Order.collectOrder(foodItemNumber);
-							System.out.println("\nVeronica=> \t Ok," + menu.getDishName(foodItemNumber) + " added, so would you like to have more (yes/no)?");
-							answerToOrderQuery=keyboard.next();
-							if(answerToOrderQuery.toLowerCase()=="no")
+							System.out.println("\nVeronica=> \t Ok," + menu.getDishName(foodItemNumber)
+									+ " added, so would you like to have more (yes/no)?");
+							answerToOrderQuery = keyboard.next();
+							if (answerToOrderQuery.toLowerCase() == "no")
 								break;
 						}
 						System.out.println("\nVeronica=> \t Your order is being prepared");
-						
-						
-						
-						
+
 					}
 
 					else {
@@ -85,17 +82,15 @@ public class UserInterface {
 						} finally {
 							System.out.println("\n Veronica=> \t Do you wish to give order?(yes/no):");
 							answerToOrderQuery = keyboard.next();
-							if (answerToOrderQuery.toLowerCase() == "yes" || answerToOrderQuery.toLowerCase().charAt(0) == 'y'
+							if (answerToOrderQuery.toLowerCase() == "yes"
+									|| answerToOrderQuery.toLowerCase().charAt(0) == 'y'
 									|| answerToOrderQuery.toLowerCase() == "yes, please."
 									|| answerToOrderQuery.toLowerCase() == "sure") {
 								System.out.println("\nVeronica=> \t Enter Order please.");
-								
+
 								System.out.println("\nVeronica=> \t From Main menu, what you would like to have?");
-								
-								
-								
-							}
-							else {
+
+							} else {
 								break;
 							}
 						}
@@ -103,13 +98,39 @@ public class UserInterface {
 					}
 
 				}
+
 			} else {
 				System.out.println("Oh, ?");
 				break;
 			}
 			break;
 		case 2:
-			
+			System.out.println("Welcome, I am Veronica, your assistant at Restaurent.");
+			System.out.println("Veronica=> \t Confirm your presence user by entering username");
+			System.out.print("User=> \t");
+			String userName = keyboard.next(); // enter admin
+			System.out.println("\nVeronica=> \t ok," + userName + " Enter your password.");
+			System.out.print("Boss => \t"); // enter admin
+			String userPassword = keyboard.next();
+			if (userName.equalsIgnoreCase("admin") && userPassword.equalsIgnoreCase("admin")) {
+				System.out.println("Veronica=> \t Welcome Boss");
+				System.out.println("Veronica=> \t What would you like to do?");
+				System.out.println("Veronica=> \t 1. Update FoodItem in Menu.");
+				System.out.println("Veronica=> \t 2. Update Raw Materials");
+				System.out.println("Veronica=> \t Enter your choice(1-2)");
+				System.out.print("Boss => \t"); 
+				int updateQuery = keyboard.nextInt();
+				switch (updateQuery) {
+				case 1:
+					break;
+				case 2:
+					break;
+				}
+
+			}
+			else {
+				System.out.println("Veronica=> \t Don't be snoopy, else I'll call for security.");
+			}
 			break;
 		default:
 			System.out.println("Pls. enter either (1/2):");
